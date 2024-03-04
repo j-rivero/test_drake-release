f9fd593fb29010685ec15819034f7bd7a458664e
Jose Luis Rivero <jrivero@osrfoundation.org>
2024-03-04 13:21:13 +0100

    Rebase from 'upstream'

--- drake-extras.cmake
+++ drake-extras.cmake
@@ -2,6 +2,12 @@
 # installed under the root of the ROS installation + /opt/drake
 # (i.e /opt/ros/rolling/opt/drake ).
 
+# drake_DIR will return the absolute path to the ROS package
+# of drake (share/drake/cmake under the ROS root installation)
+# The relative path should drive us to the root of the drake
+# upstream installation done under ROS.
+set(drake_path "${drake_DIR}/../../../opt/drake")
+
 # Since the upstream installation has the same CMake module name
 # than the ROS package we need to unset the cache variable
 # that points to the ROS package to be sure to force the
@@ -9,13 +15,4 @@
 # paths
 unset(drake_DIR CACHE)
 
-# drake_DIR will return the absolute path to the ROS package
-# of drake (share/drake/cmake under the ROS root installation)
-# The relative path should drive us to the root of the drake
-# upstream installation done under ROS.
-message(STATUS "drake_path: ${drake_path}")
-get_filename_component(abs_path ${drake_path} ABSOLUTE)
-message(STATUS "Absolute path of drake_path: ${abs_path}")
-
-set(drake_path "${drake_DIR}/../../../opt/drake")
 find_package(drake PATHS ${drake_path} NO_DEFAULT_PATH)
